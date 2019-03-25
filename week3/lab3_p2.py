73a7229db2c2fc2abf46a5b865d193d3fab56eb3
Sebastian Jeong <sebastianrcnt@gmail.com>
2019-03-25 14:16:20 +0900

    added comments

--- week3/lab3_p2.py
+++ week3/lab3_p2.py
@@ -1,6 +1,10 @@
+# get user input for EAN
 ans = input('Enter the first 12 digits of an EAN: ')
+
+# calculate check digit
 firstSum = int(ans[1]) + int(ans[3]) + int(ans[5]) + int(ans[7]) + int(ans[9]) + int(ans[11])
 secondSum = int(ans[0]) + int(ans[2]) + int(ans[4]) + int(ans[6]) + int(ans[8]) + int(ans[10])
 checkDigit = 9 - ((firstSum * 3 + secondSum - 1) % 10)
 
+# print it
 print(f'Check digit: {checkDigit}')
--- week3/lab3_p4.py
+++ week3/lab3_p4.py
@@ -1,5 +1,12 @@
+# import modules
 import random
+
+# make a list to store the generated integers
 numbers = []
+
+# add integers to numbers
 for i in range(6):
 	numbers.append(random.randint(1,45))
+
+# print numbers
 print(f'Lotto numbers of the week: {numbers[0]} {numbers[1]} {numbers[2]} {numbers[3]} {numbers[4]} {numbers[5]}')
